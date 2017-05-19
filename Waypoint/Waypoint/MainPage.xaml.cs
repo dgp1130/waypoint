b29c51374825dfc2881bfad4c02946aa877cdf98
dhchan <daniel_chan@comcast.net>
2017-05-18 18:14:38 -0700

    first camera commit

--- Waypoint/Waypoint/MainPage.xaml.cs
+++ Waypoint/Waypoint/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Plugin.Media;
 
 namespace Waypoint
 {
@@ -11,7 +12,44 @@ namespace Waypoint
     {
         public MainPage()
         {
+			Button TakePictureButton = new Button
+			{
+				Text = "Take from camera",
+			};
+			TakePictureButton.Clicked += TakePictureButton_Clicked;
+			Button UploadPictureButton = new Button
+			{
+				Text = "Pick a photo",
+			};
+			UploadPictureButton.Clicked += UploadPictureButton_Clicked;
+			Image Image1 = new Image
+			{
+				HeightRequest = 240
+			};
             InitializeComponent();
+			this.Content = new StackLayout
+			{
+				Children = {
+					TakePictureButton,
+					UploadPictureButton
+				}
+			};
         }
+
+		private void UploadPictureButton_Clicked(object sender, EventArgs e)
+		{
+
+		}
+
+		private async void TakePictureButton_Clicked(object sender, EventArgs e)
+		{
+			await CrossMedia.Current.Initialize();
+
+			if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+			{
+				await DisplayAlert("No Camera", "No Camera available.", "OK");
+				return;
+			}
+		}
     }
 }
