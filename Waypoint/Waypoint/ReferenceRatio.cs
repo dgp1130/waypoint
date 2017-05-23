49be8a7f17a89eacf482d9e238599114a670e3d2
Douglas Parker <dgp1130422@gmail.com>
2017-05-23 00:17:46 -0700

    Added parsing of OCR recognized text to create reference lines on the map image itself. Also setup map to scale and center correctly on the screen.

---
+++ Waypoint/Waypoint/ReferenceRatio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waypoint
+{
+    /// <summary>
+    /// Model class representing a ratio used as a reference for the map.
+    /// Correlates a pixel value as equivalent to a polar value for a given PolarAxis.
+    /// </summary>
+    class ReferenceRatio
+    {
+        private readonly int pixel;
+        public int Pixel { get { return pixel; } }
+
+        private readonly float polar;
+        public float Polar { get { return polar; } }
+
+        private readonly PolarAxis axis;
+        public PolarAxis Axis { get { return axis; } }
+
+        public ReferenceRatio(int pixel, float polar, PolarAxis axis)
+        {
+            this.pixel = pixel;
+            this.polar = polar;
+            this.axis = axis;
+        }
+
+        // Return a new ReferenceRatio scaling the Pixel value with the given ratio
+        public ReferenceRatio scale(double ratio)
+        {
+            return new ReferenceRatio((int) (pixel * ratio), polar, axis);
+        }
+
+        public override string ToString()
+        {
+            return $"ReferenceRatio {{ pixel = {pixel}, polar = {polar}, axis = {axis} }}";
+        }
+
+        public enum PolarAxis
+        {
+            Latitude,
+            Longitude
+        }
+    }
+}
